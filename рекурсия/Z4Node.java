1aaf65abf925f88b5c6866e133025dd1ddcf84d8
pppppplk <58656331+pppppplk@users.noreply.github.com>
2020-11-17 11:49:51 +0300

    Add files via upload

---
+++ рекурсия/Z4Node.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Z4Node {
+    int value;
+    Z4Node left;
+    Z4Node right;
+
+    public Z4Node(int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+}
