8ee713819550ca39171cf9d1b39a52bb49daf521
pppppplk <58656331+pppppplk@users.noreply.github.com>
2021-03-01 09:18:05 +0300

    добавление css

---
+++ семинар 1.03.21(javafx+css)/src/program/controllers/PersonEditingController.java
@@ -0,0 +1,126 @@
+package program.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import program.modules.Person;
+import program.utils.DateUtil;
+
+public class PersonEditingController {
+
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField streetField;
+    @FXML
+    private TextField postalCodeField;
+    @FXML
+    private TextField cityField;
+    @FXML
+    private TextField birthdayField;
+
+
+    private Stage dialogStage;
+    private Person person;
+    private boolean okClicked = false;
+
+    @FXML
+    private void initialize() {
+    }
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+
+        firstNameField.setText(person.getFirstName());
+        lastNameField.setText(person.getLastName());
+        streetField.setText(person.getStreet());
+        postalCodeField.setText(Integer.toString(person.getPostalCode()));
+        cityField.setText(person.getCity());
+        birthdayField.setText(DateUtil.format(person.getBirthDay()));
+        birthdayField.setPromptText("dd.mm.yyyy");
+    }
+
+    public boolean isOkClicked() {
+        return okClicked;
+    }
+
+    @FXML
+    private void handleOk() {
+        if (isInputValid()) {
+            person.setFirstName(firstNameField.getText());
+            person.setLastName(lastNameField.getText());
+            person.setStreet(streetField.getText());
+            person.setPostalCode(Integer.parseInt(postalCodeField.getText()));
+            person.setCity(cityField.getText());
+            person.setBirthDay(DateUtil.parse(birthdayField.getText()));
+
+            okClicked = true;
+            dialogStage.close();
+        }
+    }
+
+    @FXML
+    private void handleCancel() {
+        dialogStage.close();
+    }
+
+    private boolean isInputValid() {
+        String errorMessage = "";
+
+        if (firstNameField.getText() == null || firstNameField.getText().length() == 0) {
+            errorMessage += "Не указано имя\n";
+        }
+        if (lastNameField.getText() == null || lastNameField.getText().length() == 0) {
+            errorMessage += "Не указана  фамилия \n";
+        }
+        if (streetField.getText() == null || streetField.getText().length() == 0) {
+            errorMessage += "Не указана улицы\n";
+        }
+
+        if (postalCodeField.getText() == null || postalCodeField.getText().length() == 0) {
+            errorMessage += "Не указан индекс\n";
+        } else {
+            // пытаемся преобразовать почтовый код в int.
+            try {
+                Integer.parseInt(postalCodeField.getText());
+            } catch (NumberFormatException e) {
+                errorMessage += "Индекс вводится числами!\n";
+            }
+        }
+
+        if (cityField.getText() == null || cityField.getText().length() == 0) {
+            errorMessage += "Нет такого города!\n";
+        }
+
+        if (birthdayField.getText() == null || birthdayField.getText().length() == 0) {
+            errorMessage += "Нет такого дня рождения!\n";
+        } else {
+            if (!DateUtil.validDate(birthdayField.getText())) {
+                errorMessage += "День рождения вводится в формате dd.mm.yyyy!\n";
+            }
+        }
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Показываем сообщение об ошибке.
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Недопустимое поле");
+            alert.setHeaderText("Введите корректно поле");
+            alert.setContentText(errorMessage);
+
+            alert.showAndWait();
+
+            return false;
+        }
+    }
+}
---
+++ семинар 1.03.21(javafx+css)/src/program/modules/Person.java
@@ -0,0 +1,77 @@
+package program.modules;
+
+import javafx.beans.property.*;
+
+import java.time.LocalDate;
+
+public class Person {
+    private StringProperty firstName;
+    private StringProperty lastName;
+    private StringProperty street;
+    private StringProperty city;
+    private IntegerProperty postalCode;
+    private ObjectProperty<LocalDate> birthDay;
+    public Person(){
+        this(null,null);
+    }
+
+    public Person(String firstName,String lastName){
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+
+        this.street = new SimpleStringProperty("улица Щербаковская,38");
+        this.city = new SimpleStringProperty("Москва");
+        this.postalCode = new SimpleIntegerProperty(12345);
+        this.birthDay = new SimpleObjectProperty<LocalDate>(LocalDate.of(1995,12,12));
+    }
+    public String getFirstName() {
+        return firstName.get();
+    }
+    public int getPostalCode() {
+        return postalCode.get();
+    }
+    public String getCity() {
+        return city.get();
+    }
+    public String getLastName() {
+        return lastName.get();
+    }
+    public LocalDate getBirthDay() {
+        return birthDay.get();
+    }
+    public String getStreet() {
+        return street.get();
+    }
+
+    public StringProperty getFirstNameProperty() {
+        return firstName;
+    }
+    public IntegerProperty getPostalCodeProperty() {
+        return postalCode;
+    }
+    public StringProperty getCityProperty() {
+        return city;
+    }
+    public StringProperty getLastNameProperty() { return lastName;}
+    public ObjectProperty<LocalDate> getBirthDayProperty() { return birthDay; }
+    public StringProperty getStreetProperty() { return street;}
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay.set(birthDay);
+    }
+    public void setCity(String city) {
+        this.city.set(city);
+    }
+    public void setPostalCode(int postalCode) {
+        this.postalCode.set(postalCode);
+    }
+    public void setStreet(String street) {
+        this.street.set(street);
+    }
+}
